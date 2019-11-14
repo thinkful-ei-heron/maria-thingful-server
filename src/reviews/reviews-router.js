42ca0caa3a7d38d0fe07d3b58ca9a8a6aeccf527
Maria Danielson <mal3905@gmail.com>
2019-11-13 20:52:13 -0500

    fixed error

--- src/reviews/reviews-router.js
+++ src/reviews/reviews-router.js
@@ -9,8 +9,8 @@ const jsonBodyParser = express.json()
 reviewsRouter
   .route('/')
   .post(requireAuth, jsonBodyParser, (req, res, next) => {
-    const { thing_id, text } = req.body
-    const newReview = { thing_id, text }
+    const { thing_id, rating,  text } = req.body
+    const newReview = { thing_id, rating, text }
 
     for (const [key, value] of Object.entries(newReview))
       if (value == null)
